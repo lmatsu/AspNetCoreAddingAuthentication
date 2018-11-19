13c7d442a65a16f59ef847d9baa0f4c068454580
Lisa Matsubara <lmatsubara@ehsd.cccounty.us>
2018-11-19 11:26:20 -0800

    Completed Module 4. Added ability to register user

---
+++ WishList/Models/AccountViewModels/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WishList.Models.AccountViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
